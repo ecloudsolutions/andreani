cf71e7b8172989b31ae4d8940c38662634a25f2d
bruno <bruno.mininno@ecloudsolutions.com>
2014-10-09 13:26:24 -0300

    Cambios release 0.1.11

--- app/code/community/Ecloud/Andreani/Model/Carrier/Andreaniurgente.php
+++ app/code/community/Ecloud/Andreani/Model/Carrier/Andreaniurgente.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version   0.1.10 04.08.2014
+ * @version   0.1.11 09.10.2014
  * @author    ecloud solutions http://www.ecloudsolutions.com <info@ecloudsolutions.com>
  * @copyright Copyright (C) 2010 - 2014 ecloud solutions ®
  */
