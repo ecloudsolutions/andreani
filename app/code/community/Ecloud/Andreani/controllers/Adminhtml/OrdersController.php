055463f242554686c1e793e582dc2528cddc9aaa
bruno <bruno.mininno@ecloudsolutions.com>
2014-10-16 13:06:16 -0300

    test

--- app/code/community/Ecloud/Andreani/controllers/Adminhtml/OrdersController.php
+++ app/code/community/Ecloud/Andreani/controllers/Adminhtml/OrdersController.php
@@ -4,10 +4,14 @@
  * @author    ecloud solutions http://www.ecloudsolutions.com <info@ecloudsolutions.com>
  * @copyright Copyright (C) 2010 - 2014 ecloud solutions ®
  */
-?><?php 
+?>
+<?php 
 class Ecloud_Andreani_Adminhtml_OrdersController extends Mage_Adminhtml_Controller_Action
 {
 	public function imponer()
 	{
 		echo "jaja";
 	}
+}
+
+?>
